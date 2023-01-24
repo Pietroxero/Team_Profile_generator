6872324dce2e20215fca73e0576fa5d99ce6555b
Pietroxero <115331791+Pietroxero@users.noreply.github.com>
2023-01-23 20:55:37 -0800

    added in functions for prompts of engineer, intern and manger in index.js

--- index.js
+++ index.js
@@ -42,8 +42,82 @@ function teamBio() {
             message: "Dev Ops Engineer's email:",
           }
         ]).then((data)=> {
-            let 
+            let dev = new dev(data.name, data.badge, data.github, data.email);
+            team.splice(team.length-1, 0, engineer.HTML());
+            teamBio();
         })
       }
+      if (data.role === 'Temp'){
+        return inquirer.prompt([
+            {
+                name: "name",
+                type: "input",
+                message: "Name of Temp Employee:",
+              },
+              {
+                name: "badge",
+                type: "input",
+                message: "Badge number for Temp Employee:",
+              },
+
+              {
+                name: "school",
+                type: "input",
+                message: "Do they have School certifications",
+              },
+              {
+    
+                name: "github",
+                type: "input",
+                message: "Temp Employee's Github Repo:",
+    
+                
+              },
+              {
+                name: "email",
+                type: "input",
+                message: "Temp Employee's email:",
+              }
+        ]).then((data)=>{
+            let temp = new temp(data.name, data.badge, data.school, data.github, data.email);
+            team.splice(team.length-1, 0, temp.HTML());
+            teamBio();
+        })
+      }
+      //return /print function needed
     });
 }
+
+//Here we will be building the manager prompt questions.
+function managerQuestions(){
+    return inquirer.prompt([
+        //Similar to the prompt above we can rinse and repeat the arrays for questions.
+        {
+            name: "name",
+            type: "input",
+            message: "Manager Name:",
+          },
+          {
+            name: "badge",
+            type: "input",
+            message: "Manager Badge Number:",
+          },
+          
+          {
+            name: "email",
+            type: "input",
+            message: "Manager's email:",
+          }
+    ])
+    
+}
+
+managerQuestions()
+    .then((data) =>{
+        const lead = new lead(data.name, data.badge, data.email);
+        team.splice(team.length-1, 0, manager.getHTML());
+        teamBio();
+    })
+
+//Here we have to print and have the html open
+
